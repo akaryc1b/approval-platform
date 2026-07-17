8026fd0efc2199b58b57dd740b7011338ea7d4d5
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-18 01:32:02 +0800

    Remove unused compiler imports

--- server-modules/approval-definition-compiler/src/main/java/io/github/akaryc1b/approval/compiler/ApprovalDslCompiler.java
+++ server-modules/approval-definition-compiler/src/main/java/io/github/akaryc1b/approval/compiler/ApprovalDslCompiler.java
@@ -7,9 +7,7 @@ import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.HexFormat;
-import java.util.List;
 import java.util.Objects;
 
 /**
