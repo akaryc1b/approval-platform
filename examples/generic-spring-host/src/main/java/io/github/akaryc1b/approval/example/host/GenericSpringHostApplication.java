5878aebc029492cc1e71146750fd3889e44ab6b0
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-18 00:54:13 +0800

    Add standalone Spring Host application

---
+++ examples/generic-spring-host/src/main/java/io/github/akaryc1b/approval/example/host/GenericSpringHostApplication.java
@@ -0,0 +1,12 @@
+package io.github.akaryc1b.approval.example.host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericSpringHostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericSpringHostApplication.class, args);
+    }
+}
