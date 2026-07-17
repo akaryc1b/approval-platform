98a87852dbc89c40d3811409c136ee10bc841d3f
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-18 05:58:38 +0800

    Wire pending task center query services

--- apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalPlatformConfiguration.java
+++ apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalPlatformConfiguration.java
@@ -2,9 +2,11 @@ package io.github.akaryc1b.approval.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.akaryc1b.approval.application.ApprovalApplicationService;
+import io.github.akaryc1b.approval.application.ApprovalTaskQueryService;
 import io.github.akaryc1b.approval.application.PurchasePaymentApplicationService;
 import io.github.akaryc1b.approval.application.port.ApprovalBusinessEventOutbox;
 import io.github.akaryc1b.approval.application.port.ApprovalProjectionStore;
+import io.github.akaryc1b.approval.application.port.ApprovalTaskQuery;
 import io.github.akaryc1b.approval.application.port.AuditEventSink;
 import io.github.akaryc1b.approval.application.port.IdempotencyGuard;
 import io.github.akaryc1b.approval.application.port.PurchasePaymentAssigneeResolver;
@@ -12,6 +14,7 @@ import io.github.akaryc1b.approval.compiler.ApprovalDslCompiler;
 import io.github.akaryc1b.approval.engine.ApprovalEngine;
 import io.github.akaryc1b.approval.engine.flowable.FlowableApprovalEngine;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcApprovalProjectionStore;
+import io.github.akaryc1b.approval.persistence.jdbc.JdbcApprovalTaskQuery;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcAuditEventSink;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcIdempotencyGuard;
 import org.flowable.engine.RepositoryService;
@@ -73,6 +76,16 @@ public class ApprovalPlatformConfiguration {
         ObjectMapper approvalPersistenceObjectMapper
     ) {
         return new JdbcApprovalProjectionStore(dataSource, approvalPersistenceObjectMapper);
+    }
+
+    @Bean
+    ApprovalTaskQuery approvalTaskQuery(DataSource dataSource) {
+        return new JdbcApprovalTaskQuery(dataSource);
+    }
+
+    @Bean
+    ApprovalTaskQueryService approvalTaskQueryService(ApprovalTaskQuery approvalTaskQuery) {
+        return new ApprovalTaskQueryService(approvalTaskQuery);
     }
 
     @Bean
