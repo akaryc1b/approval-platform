c06cc48cca6af70cf1931ec69738e17e010c9c30
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-18 06:14:35 +0800

    Wire approval timeline query service

--- apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalPlatformConfiguration.java
+++ apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalPlatformConfiguration.java
@@ -3,10 +3,12 @@ package io.github.akaryc1b.approval.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.akaryc1b.approval.application.ApprovalApplicationService;
 import io.github.akaryc1b.approval.application.ApprovalTaskQueryService;
+import io.github.akaryc1b.approval.application.ApprovalTimelineQueryService;
 import io.github.akaryc1b.approval.application.PurchasePaymentApplicationService;
 import io.github.akaryc1b.approval.application.port.ApprovalBusinessEventOutbox;
 import io.github.akaryc1b.approval.application.port.ApprovalProjectionStore;
 import io.github.akaryc1b.approval.application.port.ApprovalTaskQuery;
+import io.github.akaryc1b.approval.application.port.ApprovalTimelineQuery;
 import io.github.akaryc1b.approval.application.port.AuditEventSink;
 import io.github.akaryc1b.approval.application.port.IdempotencyGuard;
 import io.github.akaryc1b.approval.application.port.PurchasePaymentAssigneeResolver;
@@ -15,6 +17,7 @@ import io.github.akaryc1b.approval.engine.ApprovalEngine;
 import io.github.akaryc1b.approval.engine.flowable.FlowableApprovalEngine;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcApprovalProjectionStore;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcApprovalTaskQuery;
+import io.github.akaryc1b.approval.persistence.jdbc.JdbcApprovalTimelineQuery;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcAuditEventSink;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcIdempotencyGuard;
 import org.flowable.engine.RepositoryService;
@@ -89,6 +92,21 @@ public class ApprovalPlatformConfiguration {
     @Bean
     ApprovalTaskQueryService approvalTaskQueryService(ApprovalTaskQuery approvalTaskQuery) {
         return new ApprovalTaskQueryService(approvalTaskQuery);
+    }
+
+    @Bean
+    ApprovalTimelineQuery approvalTimelineQuery(
+        DataSource dataSource,
+        ObjectMapper approvalPersistenceObjectMapper
+    ) {
+        return new JdbcApprovalTimelineQuery(dataSource, approvalPersistenceObjectMapper);
+    }
+
+    @Bean
+    ApprovalTimelineQueryService approvalTimelineQueryService(
+        ApprovalTimelineQuery approvalTimelineQuery
+    ) {
+        return new ApprovalTimelineQueryService(approvalTimelineQuery);
     }
 
     @Bean
