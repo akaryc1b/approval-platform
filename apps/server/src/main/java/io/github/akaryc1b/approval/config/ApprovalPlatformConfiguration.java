69cdd70dc98289b064fe5e7a507376e18c901f01
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-18 01:27:09 +0800

    Wire RepositoryService into the Flowable engine adapter

--- apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalPlatformConfiguration.java
+++ apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalPlatformConfiguration.java
@@ -3,6 +3,7 @@ package io.github.akaryc1b.approval.config;
 import io.github.akaryc1b.approval.application.ApprovalApplicationService;
 import io.github.akaryc1b.approval.engine.ApprovalEngine;
 import io.github.akaryc1b.approval.engine.flowable.FlowableApprovalEngine;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +13,12 @@ import org.springframework.context.annotation.Configuration;
 public class ApprovalPlatformConfiguration {
 
     @Bean
-    ApprovalEngine approvalEngine(RuntimeService runtimeService, TaskService taskService) {
-        return new FlowableApprovalEngine(runtimeService, taskService);
+    ApprovalEngine approvalEngine(
+        RepositoryService repositoryService,
+        RuntimeService runtimeService,
+        TaskService taskService
+    ) {
+        return new FlowableApprovalEngine(repositoryService, runtimeService, taskService);
     }
 
     @Bean
