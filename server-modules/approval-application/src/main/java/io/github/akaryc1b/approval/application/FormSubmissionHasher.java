1f10917f6273d82e6e54b0d5555969e82a2ad5e3
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-18 17:07:06 +0800

    Remove unused submission hasher import

--- server-modules/approval-application/src/main/java/io/github/akaryc1b/approval/application/FormSubmissionHasher.java
+++ server-modules/approval-application/src/main/java/io/github/akaryc1b/approval/application/FormSubmissionHasher.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HexFormat;
