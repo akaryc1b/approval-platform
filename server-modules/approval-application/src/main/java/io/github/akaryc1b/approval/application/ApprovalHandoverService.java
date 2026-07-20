076a3ebf640300bee60ca1b7d4c26ec204c21926
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-20 12:28:28 +0800

    fix: remove unused handover import

--- server-modules/approval-application/src/main/java/io/github/akaryc1b/approval/application/ApprovalHandoverService.java
+++ server-modules/approval-application/src/main/java/io/github/akaryc1b/approval/application/ApprovalHandoverService.java
@@ -24,7 +24,6 @@ import java.time.Clock;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HexFormat;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
