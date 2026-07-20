93d53e6071f311209f5e9a2fa77102b5d161d429
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-07-20 11:02:23 +0800

    feat: activate delegated task engine

--- apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalDelegationConfiguration.java
+++ apps/server/src/main/java/io/github/akaryc1b/approval/config/ApprovalDelegationConfiguration.java
@@ -1,12 +1,18 @@
 package io.github.akaryc1b.approval.config;
 
 import io.github.akaryc1b.approval.application.ApprovalDelegationService;
+import io.github.akaryc1b.approval.application.DelegatingApprovalEngine;
 import io.github.akaryc1b.approval.application.port.ApprovalDelegationStore;
+import io.github.akaryc1b.approval.application.port.ApprovalTaskDelegationAssignmentStore;
 import io.github.akaryc1b.approval.application.port.AuditEventSink;
 import io.github.akaryc1b.approval.application.port.IdempotencyGuard;
+import io.github.akaryc1b.approval.engine.ApprovalEngine;
 import io.github.akaryc1b.approval.persistence.jdbc.JdbcApprovalDelegationStore;
+import io.github.akaryc1b.approval.persistence.jdbc.JdbcApprovalTaskDelegationAssignmentStore;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 import java.time.Clock;
@@ -21,6 +27,13 @@ public class ApprovalDelegationConfiguration {
     }
 
     @Bean
+    ApprovalTaskDelegationAssignmentStore approvalTaskDelegationAssignmentStore(
+        DataSource dataSource
+    ) {
+        return new JdbcApprovalTaskDelegationAssignmentStore(dataSource);
+    }
+
+    @Bean
     ApprovalDelegationService approvalDelegationService(
         IdempotencyGuard idempotencyGuard,
         ApprovalDelegationStore approvalDelegationStore,
@@ -30,6 +43,25 @@ public class ApprovalDelegationConfiguration {
         return new ApprovalDelegationService(
             idempotencyGuard,
             approvalDelegationStore,
+            auditEventSink,
+            approvalClock,
+            UUID::randomUUID
+        );
+    }
+
+    @Bean
+    @Primary
+    ApprovalEngine delegatedApprovalEngine(
+        @Qualifier("approvalEngine") ApprovalEngine approvalEngine,
+        ApprovalDelegationStore approvalDelegationStore,
+        ApprovalTaskDelegationAssignmentStore approvalTaskDelegationAssignmentStore,
+        AuditEventSink auditEventSink,
+        Clock approvalClock
+    ) {
+        return new DelegatingApprovalEngine(
+            approvalEngine,
+            approvalDelegationStore,
+            approvalTaskDelegationAssignmentStore,
             auditEventSink,
             approvalClock,
             UUID::randomUUID
