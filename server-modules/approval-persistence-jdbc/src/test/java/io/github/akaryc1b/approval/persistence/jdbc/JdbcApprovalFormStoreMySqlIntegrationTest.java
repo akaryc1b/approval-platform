69d7581996caf4294b84cea4a5202212bbe45a37
akaryc1b <43636157+akaryc1b@users.noreply.github.com>
2026-08-14 18:29:15 +0800

    test(db-compat): run form store parity on trusted MySQL posture

--- server-modules/approval-persistence-jdbc/src/test/java/io/github/akaryc1b/approval/persistence/jdbc/JdbcApprovalFormStoreMySqlIntegrationTest.java
+++ server-modules/approval-persistence-jdbc/src/test/java/io/github/akaryc1b/approval/persistence/jdbc/JdbcApprovalFormStoreMySqlIntegrationTest.java
@@ -68,15 +68,7 @@ class JdbcApprovalFormStoreMySqlIntegrationTest {
         .withDatabaseName("approval_mysql_form_store")
         .withUsername("approval")
         .withPassword("approval")
-        .withCommand(
-            "--default-time-zone=+00:00",
-            "--character-set-server=utf8mb4",
-            "--collation-server=utf8mb4_0900_as_cs",
-            "--transaction-isolation=READ-COMMITTED",
-            "--innodb-strict-mode=ON",
-            "--sql-mode=STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,"
-                + "NO_ENGINE_SUBSTITUTION"
-        );
+        .withCommand(MySql84ProductionTestServer.command());
 
     private static DataSource dataSource;
     private static JdbcTemplate jdbc;
